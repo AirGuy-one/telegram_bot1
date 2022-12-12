b9431561ef27a50bc1beb03a54deddaed68c3e66
AirGuy-one <airguy-one@yandex.ru>
2022-12-12 19:10:59 +0300

    Add error handling

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import requests
 
 DEVMAN_TOKEN='Token d93df5c85681512862e57e04228d6fcf080df87e'
 
+payload = {'Authorization': DEVMAN_TOKEN}
+
 while True:
-    response = requests.get('https://dvmn.org/api/long_polling/', headers={'Authorization': DEVMAN_TOKEN})
-    
+    try:
+        response = requests.get('https://dvmn.org/api/long_polling/', headers=payload, timeout=5)
+    except (requests.exceptions.ReadTimeout, ConnectionError) as e:
+        print('The interval of 30 seconds has passed...')
+
 
 print(response.text)
 
