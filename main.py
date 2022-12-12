1f5160faac29882c4aab1b6b01f389366ef789cf
AirGuy-one <airguy-one@yandex.ru>
2022-12-12 19:15:50 +0300

    Add bot that send to me message 'Hello, my_name'

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import requests
+import telegram
 
 DEVMAN_TOKEN='Token d93df5c85681512862e57e04228d6fcf080df87e'
+BOT_TOKEN='5908796517:AAGXALrwq_Y2uYH8_4yp1wnhdIZohKC_TME'
+
+bot = telegram.Bot(token=BOT_TOKEN)
 
 payload = {'Authorization': DEVMAN_TOKEN}
 
-while True:
-    try:
-        response = requests.get('https://dvmn.org/api/long_polling/', headers=payload, timeout=5)
-    except (requests.exceptions.ReadTimeout, ConnectionError) as e:
-        print('The interval of 30 seconds has passed...')
+# while True:
+#     try:
+#         response = requests.get('https://dvmn.org/api/long_polling/', headers=payload, timeout=5)
+#     except (requests.exceptions.ReadTimeout, ConnectionError) as e:
+#         print('The interval of 30 seconds has passed...')
 
+user_chat_id = 746935610
 
-print(response.text)
+name = 'Billy'
+
+bot.send_message(chat_id=user_chat_id,
+                text=f"Hello {name}",
+                parse_mode=telegram.ParseMode.HTML)
 
