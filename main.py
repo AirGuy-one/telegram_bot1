e4ce29400f88c214ead6225bc1bf86d3e81c0bd6
AirGuy-one <airguy-one@yandex.ru>
2022-12-12 19:09:13 +0300

    Add while True loop

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 
 DEVMAN_TOKEN='Token d93df5c85681512862e57e04228d6fcf080df87e'
 
-response = requests.get('https://dvmn.org/api/long_polling/', headers={'Authorization': DEVMAN_TOKEN})
+while True:
+    response = requests.get('https://dvmn.org/api/long_polling/', headers={'Authorization': DEVMAN_TOKEN})
+    
 
 print(response.text)
 
