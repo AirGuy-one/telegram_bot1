41503ac5e93b10744ceee20a129904c367e3b31b
AirGuy-one <airguy-one@yandex.ru>
2022-12-12 19:08:21 +0300

    Use Long Polling API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 
 DEVMAN_TOKEN='Token d93df5c85681512862e57e04228d6fcf080df87e'
 
-response = requests.get('https://dvmn.org/api/user_reviews/', headers={'Authorization': DEVMAN_TOKEN})
+response = requests.get('https://dvmn.org/api/long_polling/', headers={'Authorization': DEVMAN_TOKEN})
 
 print(response.text)
+
