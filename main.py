d7dc1f1c08397cd8bff83f37aac313be379481f2
AirGuy-one <airguy-one@yandex.ru>
2022-12-12 19:29:39 +0300

    Add checking the status of the code

--- main.py
+++ main.py
@@ -23,21 +23,22 @@ def main():
     while True:
         try:
             response = requests.get('https://dvmn.org/api/long_polling/', headers=payload, timeout=30)
-            response_dict = json.loads(response.text)
-            if response_dict['status'] == 'found':
-                print(response_dict)
-                title_of_project = response_dict['new_attempts'][0]['lesson_title']
-                work_status = response_dict['new_attempts'][0]['is_negative']
+            if response.status_code:
+                response_dict = json.loads(response.text)
+                if response_dict['status'] == 'found':
+                    print(response_dict)
+                    title_of_project = response_dict['new_attempts'][0]['lesson_title']
+                    work_status = response_dict['new_attempts'][0]['is_negative']
 
-                if work_status:
-                    work_response = 'К сожалению, в работе нашлись ошибки.'
-                else:
-                    work_response = "Преподователю все понравилось, можно приступать к следующему уроку!"
+                    if work_status:
+                        work_response = 'К сожалению, в работе нашлись ошибки.'
+                    else:
+                        work_response = "Преподователю все понравилось, можно приступать к следующему уроку!"
 
-                bot.send_message(chat_id=user_chat_id,
-                                 text=f"У вас проверили работу \"{title_of_project}\"\n\n"
-                                      f"{work_response}",
-                                 parse_mode=telegram.ParseMode.HTML)
+                    bot.send_message(chat_id=user_chat_id,
+                                     text=f"У вас проверили работу \"{title_of_project}\"\n\n"
+                                          f"{work_response}",
+                                     parse_mode=telegram.ParseMode.HTML)
         except (requests.exceptions.ReadTimeout, ConnectionError) as e:
             print('The interval of 30 seconds has passed...')
 
